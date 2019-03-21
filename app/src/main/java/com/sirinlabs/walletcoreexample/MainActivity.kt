01fb7a26c61b29c3490fbdfcd70c63fb6b60698f
Eddy WM <eddywmdev@gmail.com>
2019-03-21 15:56:21 +0300

    Added working test case

--- app/src/main/java/com/sirinlabs/walletcoreexample/MainActivity.kt
+++ app/src/main/java/com/sirinlabs/walletcoreexample/MainActivity.kt
@@ -3,9 +3,15 @@ package com.sirinlabs.walletcoreexample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import wallet.core.jni.CoinType
 import wallet.core.jni.HDWallet
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
+
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,16 @@ class MainActivity : AppCompatActivity() {
 
         val hdWallet = HDWallet(words, password)
 
-        Log.d("test", hdWallet.seed().toString())
+
+        val privateKeyHex = hdWallet.getKeyForCoin(CoinType.ETHEREUM).data().toHex()
+
+        // https://iancoleman.io/bip39/
+        Log.d("TEST_WALLET_CORE", privateKeyHex)
+    }
+
+
+    // This is just helper function to transform a ByteArray to a Hex string
+    private fun ByteArray.toHex(): String {
+        return BigInteger(1, this).toString(16)
     }
 }
